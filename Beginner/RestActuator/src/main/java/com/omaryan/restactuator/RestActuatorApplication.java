6c24d7af7b483b87d16f41ad3b322f5783ec6df3
Om Aryan <aryan2003k@gmail.com>
2023-10-27 11:01:54 +0530

    REST w/ Actuator

---
+++ Beginner/RestActuator/src/main/java/com/omaryan/restactuator/RestActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.omaryan.restactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestActuatorApplication.class, args);
+	}
+
+}
---
+++ Beginner/RestActuator/src/test/java/com/omaryan/restactuator/RestActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omaryan.restactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
